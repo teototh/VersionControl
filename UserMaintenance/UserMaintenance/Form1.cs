e01834b6be4d58ad28db1691d4137bbc53421a76
Teodóra Tóth <teodora.toth2@stud.uni-corvinus.hu>
2019-11-23 12:58:49 +0100

    hozzáadás funkció

--- UserMaintenance/UserMaintenance/Form1.cs
+++ UserMaintenance/UserMaintenance/Form1.cs
@@ -7,17 +7,37 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using UserMaintenance.Entities;
 
 namespace UserMaintenance
 {
     public partial class Form1 : Form
     {
+        BindingList<User> users = new BindingList<User>();
         public Form1()
         {
             InitializeComponent();
+
+            //label
             lblLastName.Text = Resource1.LastName;
             lblFirstName.Text = Resource1.FirstName;
             btnAdd.Text = Resource1.Add;
+
+            //listbox
+            listUsers.DataSource = users;
+            listUsers.ValueMember = "ID";
+            listUsers.DisplayMember = "FullName";
+        }
+
+        private void btnAdd_Click(object sender, EventArgs e)
+        {
+            var u = new User()
+            {
+                LastName = txtLastName.Text,
+                FirstName=txtFirstName.Text
+            };
+
+            users.Add(u);
         }
     }
 }
